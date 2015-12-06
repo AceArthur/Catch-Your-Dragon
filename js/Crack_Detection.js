50d0be7c7391ce848523628a6458900059b3c21c
Hao Fan <haofan1126@gmail.com>
2015-12-05 22:24:29 -0500

    Regularized Code

--- js/Crack_Detection.js
+++ js/Crack_Detection.js
@@ -1,27 +1,24 @@
 function crackDetection(top, left){
-	var signal = false;
-	if(Crack_List.length > 0){
-		for(var i = 0; i < Crack_List.length; i++){
-			for(var j = 0; j < Crack_List[i].Edge_List.length; j++){
-				var point_1 = Crack_List[i].Edge_List[j].Point_List[0];
-				var point_2 = Crack_List[i].Edge_List[j].Point_List[1];
-				// threshold
-				var safetyDistance = 10;
-				var x2 = point_2.top;
-				var y2 = point_2.left;
-				var x1 = point_1.top;
-				var y1 = point_1.left;
-				var x0 = top;
-				var y0 = left;
-				var distance = (Math.abs((y2 - y1) * x0 +(x1 - x2) * y0 + ((x2 * y1) -(x1 * y2)))) /
-					(Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x1 - x2, 2)));
-				if(distance <= (safetyDistance + 50)){
-					signal = true;
-					return signal;
-				}
-			}
-		}
+  var signal = false;
+  for(var i = 0; i < Crack_List.length; i++){
+    for(var j = 0; j < Crack_List[i].Edge_List.length; j++){
+      var point_1 = Crack_List[i].Edge_List[j].Point_List[0];
+      var point_2 = Crack_List[i].Edge_List[j].Point_List[1];
+	  var safetyDistance = 10;
+	  var x2 = point_2.top;
+	  var y2 = point_2.left;
+	  var x1 = point_1.top;
+	  var y1 = point_1.left;
+	  var x0 = top;
+	  var y0 = left;
+	  var distance = (Math.abs((y2 - y1) * x0 +(x1 - x2) * y0 + ((x2 * y1) -(x1 * y2)))) / (Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x1 - x2, 2)));
+	  if(distance <= (safetyDistance + 50)){
+		signal = true;
+		return signal;
+	  }
 	}
-                
-	return signal;
+  }
+  if(Crack_List.length <= 0){
+    return signal;
+  }
 }
--- js/Make_Position.js
+++ js/Make_Position.js
@@ -1,37 +1,29 @@
-function makeNewPosition(top, left){
-                                      
-	// Get viewport dimensions (remove the dimension of the div)
-	//var h = top + 50;
-	//var w = left + 50;
-                                      
-	if(changeDirection(dest_x, dest_y) == true){
-		goForword = true;
-	}
-	if(goForword == true){
-		//alert("NEW DIRECTION");
-		var h = $(window).height() - 50;
-		var w = $(window).width() - 50;
-		var n_top = Math.floor(Math.random() * h);
-		var n_left = Math.floor(Math.random() * w);
-		while(n_top == top && n_left == left){
-			var n_top = Math.floor(Math.random() * h);
-			var n_left = Math.floor(Math.random() * w);
-		}
-		//var newGoal = setNewDirection();
-		//dest_x = newGoal.n_top;
-		//dest_y = newGoal.n_left;
-        dest_x = n_top;
-        dest_y = n_left;
-        //alert(dest_x);
-        slope_sin = (dest_y - left)/Math.sqrt(Math.pow(dest_y - left, 2) + Math.pow(dest_x - top, 2));
-        slope_cos = (dest_x - top)/Math.sqrt(Math.pow(dest_y - left, 2) + Math.pow(dest_x - top, 2));
-        goForword = false;
-	}
-                                      
-                                      
-		//alert(slope_cos, slope_sin);
-		var nh = top + 20 * slope_cos;
-		var nw = left + 20 * slope_sin;
-		//COUNT--;
-		return [nh, nw];
+var goForword = true;
+var dest_x;
+var dest_y;
+var slope_cos;
+var slope_sin;
+
+function makeNewPosition(top, left){                                    
+  if(changeDirection(dest_x, dest_y) === true){
+    goForword = true;
+  }
+  if(goForword === true){
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    var n_top = Math.floor(Math.random() * h);
+    var n_left = Math.floor(Math.random() * w);
+    while(n_top === top && n_left == left){
+      n_top = Math.floor(Math.random() * h);
+      n_left = Math.floor(Math.random() * w);
+    }
+    dest_x = n_top;
+    dest_y = n_left;
+    slope_sin = (dest_y - left)/Math.sqrt(Math.pow(dest_y - left, 2) + Math.pow(dest_x - top, 2));
+    slope_cos = (dest_x - top)/Math.sqrt(Math.pow(dest_y - left, 2) + Math.pow(dest_x - top, 2));
+    goForword = false;
+  }
+  var nh = top + 20 * slope_cos;
+  var nw = left + 20 * slope_sin;
+  return [nh, nw];
 }
